8958d0079cb28af33eaf8d221fd9048d80a6cfdd
Ravi Kumar <ravikr12082018@gmail.com>
2024-12-31 09:27:57 +0530

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,37 +5,40 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <>
+        <div className="bg-blue-950">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
-                        <img src={logo} className="h-8 me-3" alt="NITP Ecell Logo" />
+                        <p className="text-2xl text-red-500 font-semibold">E-CELL NIT PATNA</p>
+                        <p className="text-gray-400">National Institute of Technology Patna, Patna, Bihar</p>
+                        <p className="text-gray-400">(800005), India</p>
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-200 uppercase">Resources</h2>
+                            <ul className="text-gray-400 font-medium">
                                 <li className="mb-4">
-                                    <Link to="#" className="hover:underline">Research Papers</Link>
+                                    <Link to="#" className="hover:underline">Events</Link>
                                 </li>
                                 <li>
-                                    <Link to="#" className="hover:underline">Our Work</Link>
+                                    <Link to="#" className="hover:underline">Gallery</Link>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-200 uppercase">Follow us</h2>
+                            <ul className="text-gray-400 font-medium">
                                 <li className="mb-4">
-                                    <Link to="https://www.instagram.com/ecell.nitpatna/" className="hover:underline ">Instagram</Link>
+                                    <Link to="#" className="hover:underline ">Github</Link>
                                 </li>
                                 <li>
-                                    <Link to="https://www.linkedin.com/company/entrepreneurship-cell-nit-patna/" className="hover:underline">LinkedIn</Link>
+                                    <Link to="#" className="hover:underline">Discord</Link>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                            <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                            <h2 className="mb-6 text-sm font-semibold text-gray-200 uppercase">Legal</h2>
+                            <ul className="text-gray-400 font-medium">
                                 <li className="mb-4">
                                     <Link to="#" className="hover:underline">Privacy Policy</Link>
                                 </li>
@@ -46,26 +49,26 @@ function Footer() {
                         </div>
                     </div>
                 </div>
-                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <hr className="my-6 border-gray-400 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <Link to="#" className="hover:underline">E-cell™</Link>. All Rights Reserved.
+                    <span className="text-sm text-gray-400 sm:text-center">© 2025 E-cell. All Rights Reserved.
                     </span>
                     <div className="flex mt-4 sm:justify-center sm:mt-0">
                         <Link to="https://www.linkedin.com/company/entrepreneurship-cell-nit-patna/" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <svg className="w-5 h-5 text-gray-400 hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path fillRule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clipRule="evenodd" />
                                 <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
                             </svg>
                             <span className="sr-only">LinkedIn</span>
                         </Link>
                         <Link to="https://www.instagram.com/ecell.nitpatna/" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <svg className="w-5 h-5 text-gray-400 hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path fill="currentColor" fillRule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clipRule="evenodd" />
                             </svg>
                             <span className="sr-only">Instagram</span>
                         </Link>
                         <Link to="https://www.facebook.com/ecellnitp/" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                            <svg className="w-4 h-4 text-gray-400 hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                                 <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
                             </svg>
                             <span className="sr-only">Facebook</span>
@@ -77,7 +80,7 @@ function Footer() {
                             <span className="sr-only">Discord community</span>
                         </Link> */}
                         <Link to="https://twitter.com/ecell_nitp" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <svg className="w-4 h-4 text-gray-400 hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                 <path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z" />
                             </svg>
                             <span className="sr-only">Twitter</span>
@@ -92,6 +95,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
+        </div>
         </>
     )
 }
